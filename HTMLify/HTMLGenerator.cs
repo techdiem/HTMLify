b850808669510460eedcdbb71df2ceac68e16fbc
techdiem <48885218+techdiem@users.noreply.github.com>
2021-12-11 21:59:14 +0100

    Initial commit

---
+++ HTMLify/HTMLGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+
+namespace HTMLify
+{
+    class HTMLGenerator
+    {
+        public string templatePath = string.Format("{0}\\template.html", Directory.GetCurrentDirectory());
+
+        private string loadTemplate()
+        {
+            string template = System.IO.File.ReadAllText(templatePath);
+            return template;
+        }
+
+        public void saveFile(string text)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".html";
+            saveFileDialog.Filter = "Webseite (.html)|*.html";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, text);
+            }
+        }
+
+        public string convertText(string text)
+        {
+            Regex r = new Regex(@"(https?://[^\s]+)");
+            text = r.Replace(text, "<a href=\"$1\">$1</a>");
+            text = text.Replace(Environment.NewLine, "<br />" + Environment.NewLine);
+            string result = loadTemplate().Replace("[[content]]", text);
+            return result;
+        }
+    }
+}
---
+++ HTMLify/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace HTMLify
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HTMLGenerator htmlGenerator = new HTMLGenerator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dockEditTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("notepad.exe", htmlGenerator.templatePath);
+        }
+
+        private void dockFileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            string richText, htmlPage;
+            TextRange textRange = new TextRange(richtextContent.Document.ContentStart, richtextContent.Document.ContentEnd);
+            richText = textRange.Text;
+            htmlPage = htmlGenerator.convertText(richText);
+            htmlGenerator.saveFile(htmlPage);
+        }
+    }
+}
